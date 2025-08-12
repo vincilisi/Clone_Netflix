4ff51e1ba8715a74553371a15ec4c237a30dcdc2
vincilisi <lisitano95@gmail.com>
2025-08-12 18:30:13 +0200

    :)

--- src/app/movie/[movie]/page.tsx
+++ src/app/movie/[movie]/page.tsx
@@ -2,21 +2,17 @@ import VetrinaFilm from '@/app/components/VetrinaFilm';
 import VetrinaSerieTV from '@/app/components/VetrinaSerie';
 import { notFound } from 'next/navigation';
 
-interface Props {
-    params: {
+// Tipi ufficiali di Next.js 15
+interface MoviePageProps {
+    params: Promise<{
         movie: string;
-    };
+    }>;
 }
 
-export default async function MoviePage({
-    params,
-}: {
-    params: { movie: string };
-}) {
-    const type = params.movie;
+export default async function MoviePage({ params }: MoviePageProps) {
+    const { movie: type } = await params;
 
     if (type === 'film') return <VetrinaFilm />;
     if (type === 'serie-tv') return <VetrinaSerieTV />;
     return notFound();
 }
-
