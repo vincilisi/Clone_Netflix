ff57f27d300b58ff840e222883d4154b8426d80e
vincilisi <lisitano95@gmail.com>
2025-08-16 17:52:13 +0200

    errori deploy

--- next.config.js
+++ next.config.js
@@ -1,23 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: { appDir: true },
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'image.tmdb.org',
-        port: '',
         pathname: '/t/p/**',
       },
     ],
   },
-  eslint: {
-    // Ignora errori di lint durante la build su Netlify
-    ignoreDuringBuilds: true,
-  },
-  experimental: {
-    appDir: true, // App Router
-  },
-  output: 'standalone', // per Netlify
 };
 
 module.exports = nextConfig;
